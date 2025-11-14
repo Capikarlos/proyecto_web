6603eaa5a65c5641635fbf6bd2fc17093803a5ea
Carlos Antonio López Prieto <lopezcarlosant@gmail.com>
2025-11-14 10:54:09 -0600

    hols

--- flask/config.py
+++ flask/config.py
@@ -3,7 +3,8 @@ from urllib.parse import quote_plus
 
 DB_USER = "carlos_user"
 DB_PASS = quote_plus("12345")
-DB_HOST = "localhost"
+#DB_HOST = "localhost"
+DB_HOST = "192.168.1.68"
 DB_NAME = "proyecto_web"
 
 class Config:
